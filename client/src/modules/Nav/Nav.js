a2ee85ab4e167af3d9de3923eeb0ff0a33e14f8d
MrQbasic <77384846+MrQbasic@users.noreply.github.com>
2024-07-24 02:56:32 +0200

    Update Nav.js

--- client/src/modules/Nav/Nav.js
+++ client/src/modules/Nav/Nav.js
@@ -4,9 +4,9 @@ import "./Nav.css"
 function Nav({navPos}){
     return(
         <div class="Nav">
-            <div className={navPos===0?("selected"):("")} onClick={()=>{localStorage.setItem("windowPosition", 0);window.location.reload()}}>Leaderboard</div>
-            <div className={navPos===1?("selected"):("")} onClick={()=>{localStorage.setItem("windowPosition", 1);window.location.reload()}}>My Uploads</div>
-            <div className={navPos===2?("selected"):("")} onClick={()=>{localStorage.setItem("windowPosition", 2);window.location.reload()}}>About</div>
+            <div className={navPos==0?("selected"):("")} onClick={()=>{localStorage.setItem("windowPosition", 0);window.location.reload()}}>Leaderboard</div>
+            <div className={navPos==1?("selected"):("")} onClick={()=>{localStorage.setItem("windowPosition", 1);window.location.reload()}}>My Uploads</div>
+            <div className={navPos==2?("selected"):("")} onClick={()=>{localStorage.setItem("windowPosition", 2);window.location.reload()}}>About</div>
         </div>
     )
 }
